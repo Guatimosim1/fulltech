8b3ffd7d9591384f3dc42983104506cc8e7ab97a
Darlan Maia <darlanmaia2010@hotmail.com>
2023-08-30 16:23:34 -0300

    DARLAN - Adicionando anotação @CrossOrigin à todos os métodos dos controllers

--- src/main/java/com/capgemini/panteranegra/restapi/restcontroller/ChairRestController.java
+++ src/main/java/com/capgemini/panteranegra/restapi/restcontroller/ChairRestController.java
@@ -1,7 +1,6 @@
 package com.capgemini.panteranegra.restapi.restcontroller;
 
 import com.capgemini.panteranegra.entities.Chair;
-import com.capgemini.panteranegra.entities.Session;
 import com.capgemini.panteranegra.models.CustomerNameInputDTO;
 import com.capgemini.panteranegra.restapi.restservice.ChairRestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +15,25 @@ public class ChairRestController {
     @Autowired
     private ChairRestService service;
 
+    @CrossOrigin
     @GetMapping("/{id}")
     public Chair findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
+    @CrossOrigin
     @GetMapping
     public List<Chair> findAll() {
         return service.findAll();
     }
 
+    @CrossOrigin
     @GetMapping("/session/{id}")
     public List<Chair> findBySessionId(@PathVariable("id") Long id) {
         return service.findBySessionId(id);
     }
 
+    @CrossOrigin
     @PutMapping("/{id}")
     public Chair assignChairToCustomer(@RequestBody CustomerNameInputDTO customerName, @PathVariable("id") Long id) {
         return service.assignChairToCustomer(customerName, id);
